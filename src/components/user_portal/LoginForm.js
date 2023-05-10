be7be291124a9b6dd2fbe701b9f26d61dac8bd46
Zilin Li <tonyli19970130@gmail.com>
2023-05-10 05:13:03 +0000

    fix missed components on user portal

--- src/components/user_portal/LoginForm.js
+++ src/components/user_portal/LoginForm.js
@@ -1,15 +1,83 @@
 /**
  * Copyright (c) 2023
  *
- * @summary Implementation of 
- * @author 
- * @date 
+ * @summary Implementation of LoginForm
+ * @author 202302 Flag Camp Team03
+ * @date 2023-05-09
  *  
  */
-const LoginForm = () => {
+
+// Project imports
+import SignupForm from "./SignupForm";
+import { login } from "../../utils/backend_utils";
+
+// React imports
+import React, { useState } from "react";
+
+// Antd imports
+import { Form, Button, Input, message } from "antd";
+import { UserOutlined, UsergroupAddOutlined } from "@ant-design/icons";
+
+const LoginForm = ({ setAuthed }) => {
+
+  const [loading, setLoading] = useState(false);
+
+  const handleFormSubmit = async (data) => {
+    setLoading(true);
+
+    try {
+
+      await login(data);
+      setAuthed(true);
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
-    <div>Hello World</div>
+    <div style={{ width: 500, margin: "20px auto" }}>
+      <Form onFinish={handleFormSubmit}>
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!",
+            },
+          ]}
+        >
+          <Input
+            disabled={loading}
+            prefix={<UserOutlined />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input.Password disabled={loading} placeholder="Password" />
+        </Form.Item>
+        <Form.Item>
+          <Button
+            loading={loading}
+            type="primary"
+            htmlType="submit"
+            style={{ width: "100%" }}
+          >
+            Log in
+          </Button>
+          <UsergroupAddOutlined /> <SignupForm />
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
--- src/components/user_portal/SignupForm.js
+++ src/components/user_portal/SignupForm.js
@@ -1,15 +1,104 @@
 /**
  * Copyright (c) 2023
  *
- * @summary Implementation of 
- * @author 
- * @date 
+ * @summary Implementation of SignupButton
+ * @author 202302 Flag Camp Team03
+ * @date 2023-05-09
  *  
  */
+
+// Project imports
+import { register } from "../../utils/backend_utils";
+
+// Antd imports
+import { Button, Modal, Form, Input, message } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+
+// React imports
+import { useState } from "react";
+
 const SignupButton = () => {
 
+  const [modalVisible, setModalVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleRegisterOnClick = () => {
+    setModalVisible(true);
+  };
+
+  const handleModalCancel = () => {
+    setModalVisible(false);
+  };
+
+  const handleFormSubmit = async (data) => {
+    setLoading(true);
+
+    try {
+      await register(data);
+      message.success("Sign up successfully");
+      setModalVisible(false);
+    } catch (error) {
+      message.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>Hello World</div>
+    <>
+      <Button
+        type="link"
+        style={{ padding: 0 }}
+        onClick={handleRegisterOnClick}
+      >
+        Join Our Membership Now!
+      </Button>
+      <Modal
+        title="Sign Up"
+        visible={modalVisible}
+        onCancel={handleModalCancel}
+        footer={null}
+      >
+        <Form onFinish={handleFormSubmit}>
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Username!",
+              },
+            ]}
+          >
+            <Input
+              disabled={loading}
+              prefix={<UserOutlined />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password!",
+              },
+            ]}
+          >
+            <Input.Password disabled={loading} placeholder="Password" />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              loading={loading}
+              type="primary"
+              htmlType="submit"
+              style={{ width: "100%" }}
+            >
+              Sign up
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
+    </>
   );
 };
 
