518dbd6d829a0efb8fe15726bf3f04b1aa72fa0b
Catherine Zhou <jiezh@ucdavis.edu>
2023-05-13 17:39:59 -0700

    styled homepage and package infor

--- src/components/delivery_plan/delivery_planning/workflow/PackageInformationStep.js
+++ src/components/delivery_plan/delivery_planning/workflow/PackageInformationStep.js
@@ -1,20 +1,10 @@
-/**
- * Copyright (c) 2023
- *
- * @summary Implementation of PackageInformationStep
- * @author Catherine Zhou
- * @date 2023-05-09
- *  
- */
-
-import { Card, Image } from "antd";
+import { Card, Image, Space } from "antd";
 import React, { useState } from "react";
-import { Form, Button, Input, message } from "antd";
+import { Form, Button, Input, message, Typography } from "antd";
 import { UserOutlined, UsergroupAddOutlined } from "@ant-design/icons";
 import { login } from "../../../../utils/backend_utils";
 
-// Project imports
-import { DELIVERY_STATE } from "./../../../../utils/delivery_plan_utils"
+const { Title } = Typography;
 
 const PackageInformationStep = ({ deliveryState }) => {
   const [loading, setLoading] = useState(false);
@@ -34,78 +24,116 @@ const PackageInformationStep = ({ deliveryState }) => {
   return (
     <Card title="PACKAGE INFORMATION" style={{ width: 800, left: 20 }}>
       <Form onFinish={handleFormSubmit}>
-        <Form.Item
-          style={{ width: 350 }}
-          label="First Name:"
-          rules={[
-            {
-              required: true,
-              message: "Please input First Name!",
-            },
-          ]}
-        >
-          <Input disabled={loading} />
-        </Form.Item>
-        <Form.Item
-          style={{ width: 350 }}
-          label="Last Name:"
-          rules={[
-            {
-              required: true,
-              message: "Please input Last Name!",
-            },
-          ]}
-        >
-          <Input disabled={loading} />
-        </Form.Item>
-        <Form.Item
-          style={{ width: 350 }}
-          label="Phone Number:"
-          rules={[
-            {
-              required: true,
-              message: "Please input Contact!",
-            },
-          ]}
-        >
-          <Input disabled={loading} />
-        </Form.Item>
-        <Form.Item
-          style={{ width: 350 }}
-          label="Email:"
-          rules={[
-            {
-              required: true,
-              message: "Please input Email!",
-            },
-          ]}
-        >
-          <Input disabled={loading} />
-        </Form.Item>
-        <Form.Item
-          style={{ width: 350 }}
-          label="Content:"
-          rules={[
-            {
-              required: true,
-              message: "Please input Content!",
-            },
-          ]}
-        >
-          <Input disabled={loading} />
-        </Form.Item>
-        <Form.Item
-          style={{ width: 350 }}
-          label="Weight:"
-          rules={[
-            {
-              required: true,
-              message: "Please input Weight!",
-            },
-          ]}
-        >
-          <Input disabled={loading} />
-        </Form.Item>
+        <Title level={5} style={{ textAlign: "left" }}>
+          Sender's Information:
+        </Title>
+        <Space style={{ textAlign: "left" }}>
+          <Form.Item
+            style={{ width: 350 }}
+            label="First Name:"
+            rules={[
+              {
+                required: true,
+                message: "Please input First Name!",
+              },
+            ]}
+          >
+            <Input disabled={loading} />
+          </Form.Item>
+          <Form.Item
+            style={{ width: 350 }}
+            label="Last Name:"
+            rules={[
+              {
+                required: true,
+                message: "Please input Last Name!",
+              },
+            ]}
+          >
+            <Input disabled={loading} />
+          </Form.Item>
+        </Space>
+        <Title level={5} style={{ textAlign: "left" }}>
+          Receiver's Information:
+        </Title>
+        <Space style={{ textAlign: "left" }}>
+          <Form.Item
+            style={{ width: 350 }}
+            label="First Name:"
+            rules={[
+              {
+                required: true,
+                message: "Please input First Name!",
+              },
+            ]}
+          >
+            <Input disabled={loading} />
+          </Form.Item>
+          <Form.Item
+            style={{ width: 350 }}
+            label="Last Name:"
+            rules={[
+              {
+                required: true,
+                message: "Please input Last Name!",
+              },
+            ]}
+          >
+            <Input disabled={loading} />
+          </Form.Item>
+        </Space>
+        <Space>
+          <Form.Item
+            style={{ width: 350 }}
+            label="Phone Number:"
+            rules={[
+              {
+                required: true,
+                message: "Please input Contact!",
+              },
+            ]}
+          >
+            <Input disabled={loading} />
+          </Form.Item>
+          <Form.Item
+            style={{ width: 350 }}
+            label="Email:"
+            rules={[
+              {
+                required: true,
+                message: "Please input Email!",
+              },
+            ]}
+          >
+            <Input disabled={loading} />
+          </Form.Item>
+        </Space>
+        <Space>
+          <Form.Item
+            style={{ width: 350 }}
+            label="Content:"
+            rules={[
+              {
+                required: true,
+                message: "Please input Content!",
+              },
+            ]}
+          >
+            <Input disabled={loading} />
+          </Form.Item>
+          <Form.Item
+            style={{ width: 350 }}
+            label="Weight:"
+            rules={[
+              {
+                required: true,
+                message: "Please input Weight!",
+              },
+            ]}
+          >
+            <Input disabled={loading} />
+          </Form.Item>
+        </Space>
         <Form.Item
           label="Shipping Address:"
           rules={[
--- src/components/user_portal/user_authentication/LoginForm.js
+++ src/components/user_portal/user_authentication/LoginForm.js
@@ -4,7 +4,7 @@
  * @summary Implementation of LoginForm
  * @author 202302 Flag Camp Team03
  * @date 2023-05-09
- *  
+ *
  */
 
 // Project imports
@@ -14,16 +14,29 @@ import { showSuccess, showError } from "../../../utils/dialog_utils";
 
 // React imports
 import React, { useState } from "react";
-import { Player } from 'video-react';
-import "video-react/dist/video-react.css"; 
+import { Player } from "video-react";
+import "video-react/dist/video-react.css";
 
 // Antd imports
-import { Form, Button, Input, Row, Col, Image, Space, Modal, Typography } from "antd";
-import { UserOutlined, UsergroupAddOutlined, PlayCircleOutlined } from "@ant-design/icons";
+import {
+  Form,
+  Button,
+  Input,
+  Row,
+  Col,
+  Image,
+  Space,
+  Modal,
+  Typography,
+} from "antd";
+import {
+  UserOutlined,
+  UsergroupAddOutlined,
+  PlayCircleOutlined,
+} from "@ant-design/icons";
 
 const { Text } = Typography;
 const LoginForm = ({ setAuthed }) => {
-
   const [player, setPlayer] = useState();
   const [showVideo, setShowVideo] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -32,7 +45,6 @@ const LoginForm = ({ setAuthed }) => {
     setLoading(true);
 
     try {
-
       await login(data);
       setAuthed(true);
       showSuccess("Success", "Login successfully ! ");
@@ -44,80 +56,97 @@ const LoginForm = ({ setAuthed }) => {
   };
 
   return (
-
     <>
-    <Modal
-      title="Delivery Plan Workflow Tutorial Demo"
-      visible={showVideo}
-      footer={null}
-      onCancel={() => setShowVideo(false)}
-      afterClose={() => player.pause()}
-      bodyStyle={{ padding: 0 }}
+      <Modal
+        title="Delivery Plan Workflow Tutorial Demo"
+        visible={showVideo}
+        footer={null}
+        onCancel={() => setShowVideo(false)}
+        afterClose={() => player.pause()}
+        bodyStyle={{ padding: 0 }}
       >
-        <Player autoPlay ref={ref => setPlayer(ref)}>
-            <source src="./demo.mp4" />
+        <Player autoPlay ref={(ref) => setPlayer(ref)}>
+          <source src="./demo.mp4" />
         </Player>
-    </Modal>
-    <Row>
-      <Col span={12}> 
-        <Image preview={false} width={1300} height={800} src={"./robot_drone_login.jpg"}/>
-      </Col>
-      <Col span={12}>
-        <Space direction="vertical" align="center" style={{marginLeft: 400}}>
-          <Image preview={false} width={200} height={200} src={"./key.png"}/>
-          <div style={{ width: 500, margin: "20px auto" }}>
-            <Form onFinish={handleFormSubmit}>
-              <Form.Item
-                name="username"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your Username!",
-                  },
-                ]}
-              >
-                <Input
-                  disabled={loading}
-                  prefix={<UserOutlined />}
-                  placeholder="Username"
-                />
-              </Form.Item>
-              <Form.Item
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your Password!",
-                  },
-                ]}
-              >
-                <Input.Password disabled={loading} placeholder="Password" />
-              </Form.Item>
-              <Form.Item>
-                <Button
-                  loading={loading}
-                  type="primary"
-                  htmlType="submit"
-                  style={{ width: "100%" }}
+      </Modal>
+      <Row>
+        <Col span={12}>
+          <Image
+            preview={false}
+            width={1300}
+            height={800}
+            src={"./robot_drone_login.jpg"}
+          />
+        </Col>
+        <Col span={12}>
+          <Space
+            direction="vertical"
+            align="center"
+            style={{ marginLeft: 400 }}
+          >
+            <Image preview={false} width={200} height={200} src={"./key.png"} />
+            <div style={{ width: 500, margin: "20px auto" }}>
+              <Form onFinish={handleFormSubmit}>
+                <Form.Item
+                  name="username"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your Username!",
+                    },
+                  ]}
                 >
-                  Log in
-                </Button>
-                <UsergroupAddOutlined /> <SignupForm />
-              </Form.Item>
-            </Form>
-          </div>
-          <Image preview={false} width={100} height={100} src={"./video.png"}/>
-          <Button icon={<PlayCircleOutlined />} type="primary" onClick={() => setShowVideo(true)}>
+                  <Input
+                    disabled={loading}
+                    prefix={<UserOutlined />}
+                    placeholder="Username"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your Password!",
+                    },
+                  ]}
+                >
+                  <Input.Password disabled={loading} placeholder="Password" />
+                </Form.Item>
+                <Form.Item>
+                  <Button
+                    loading={loading}
+                    type="primary"
+                    htmlType="submit"
+                    style={{ width: "100%" }}
+                  >
+                    Log in
+                  </Button>
+                  <UsergroupAddOutlined /> <SignupForm />
+                </Form.Item>
+              </Form>
+            </div>
+            <Image
+              preview={false}
+              width={100}
+              height={100}
+              src={"./video.png"}
+            />
+            <Button
+              icon={<PlayCircleOutlined />}
+              type="primary"
+              onClick={() => setShowVideo(true)}
+            >
               Play Demo
-          </Button>
-          <Text style={{ fontSize: 15, color: "grey" }}>
-            First time here? We provide a tutorial demo on our delivery plan workflow .
-          </Text>
-        </Space>
-      </Col>
-    </Row>
+            </Button>
+            <Text style={{ fontSize: 15, color: "grey" }}>
+              First time here? We provide a tutorial demo on our delivery plan
+              workflow .
+            </Text>
+          </Space>
+        </Col>
+      </Row>
     </>
   );
 };
-
 export default LoginForm;
